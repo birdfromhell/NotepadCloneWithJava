6c82655e0eb5979581005367e30a16dc031584b2
Ababil Mustax <95958484+birdfromhell@users.noreply.github.com>
2023-10-12 18:57:54 +0700

    Refactor Notepad application and add functional file operations
    The Notepad application has been significantly refactored to improve readability and code structure. The menu bar has been streamlined and includes a new 'Help' menu option with an 'About' submenu item. The main text area now provides scrolling bars when necessary. The 'Notepad' icon resource and resizable feature have been removed for simplification.
    
    In addition, the 'New File', 'Open File', 'Save File', 'Print', 'Exit', 'Cut', 'Copy', 'Paste', 'Select All', and 'About' actions are now functional. After selecting these menu options, the corresponding functions - creating a new file, opening an existing file, saving the current file, printing the file, exiting the application, cutting or copying selected text, pasting text into the text area, selecting all text, and displaying 'About' information - are successfully performed. These improvements enhance the usability of the Notepad application.

--- src/Notepad.java
+++ src/Notepad.java
@@ -1,12 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 
-public class Notepad extends  JFrame  implements ActionListener {
+public class Notepad extends JFrame implements ActionListener {
     JMenuBar Barmenu = new JMenuBar();
     JMenu file = new JMenu("File");
     JMenu edit = new JMenu("Edit");
-    JMenu help = new JMenu("About");
+    JMenu help = new JMenu("Help");
 
     JMenuItem newfile = new JMenuItem("New File");
     JMenuItem openfile = new JMenuItem("Open File");
@@ -14,23 +15,19 @@ public class Notepad extends  JFrame  implements ActionListener {
     JMenuItem print = new JMenuItem("Print");
     JMenuItem exit = new JMenuItem("Exit");
 
-
-
-
     JMenuItem cut = new JMenuItem("Cut");
     JMenuItem copy = new JMenuItem("Copy");
     JMenuItem paste = new JMenuItem("Paste");
     JMenuItem all = new JMenuItem("Select All");
 
+    JMenuItem about = new JMenuItem("About");
+
     JTextArea areatext = new JTextArea();
 
     Notepad() {
         setTitle("Notepad Ku");
-        setResizable(true);
         setBounds(50, 50, 800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ImageIcon icon = new ImageIcon(getClass().getResource("Notepad.png"));
-        setIconImage(icon.getImage());
 
         setJMenuBar(Barmenu);
         Barmenu.add(file);
@@ -48,13 +45,13 @@ public class Notepad extends  JFrame  implements ActionListener {
         edit.add(paste);
         edit.add(all);
 
-
+        help.add(about);
 
         JScrollPane scroll = new JScrollPane(areatext);
         add(scroll);
-        areatext.setFont(new Font("SAN_SERIF" ,Font.PLAIN, 20));
-        scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+        areatext.setFont(new Font("SAN_SERIF", Font.PLAIN, 20));
+        scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         areatext.setLineWrap(true);
         areatext.setWrapStyleWord(true);
 
@@ -63,13 +60,69 @@ public class Notepad extends  JFrame  implements ActionListener {
         savefile.addActionListener(this);
         print.addActionListener(this);
         exit.addActionListener(this);
+        cut.addActionListener(this);
         copy.addActionListener(this);
+        paste.addActionListener(this);
         all.addActionListener(this);
 
+        about.addActionListener(this);
     }
 
     public static void main(String[] args) {
-
         new Notepad().setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        if (command.equals(newfile.getActionCommand())) {
+            areatext.setText("");
+        } else if (command.equals(openfile.getActionCommand())) {
+            JFileChooser chooser = new JFileChooser();
+            int chooserOption = chooser.showOpenDialog(null);
+            if (chooserOption == JFileChooser.APPROVE_OPTION) {
+                File f = chooser.getSelectedFile();
+                try {
+                    BufferedReader br = new BufferedReader(new FileReader(f));
+                    areatext.read(br, null);
+                    br.close();
+                    areatext.requestFocus();
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, ex);
+                }
+            }
+        } else if (command.equals(savefile.getActionCommand())) {
+            JFileChooser fc = new JFileChooser();
+            int chooserOption = fc.showSaveDialog(null);
+            if (chooserOption == JFileChooser.APPROVE_OPTION) {
+                try {
+                    FileWriter fw = new FileWriter(fc.getSelectedFile().getAbsolutePath());
+                    BufferedWriter bw = new BufferedWriter(fw);
+                    areatext.write(bw);
+                    bw.close();
+                    areatext.requestFocus();
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, ex);
+                }
+            }
+        } else if (command.equals(print.getActionCommand())) {
+            try {
+                areatext.print();
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, ex);
+            }
+        } else if (command.equals(exit.getActionCommand())) {
+            System.exit(0);
+        } else if (command.equals(cut.getActionCommand())) {
+            areatext.cut();
+        } else if (command.equals(copy.getActionCommand())) {
+            areatext.copy();
+        } else if (command.equals(paste.getActionCommand())) {
+            areatext.paste();
+        } else if (command.equals(all.getActionCommand())) {
+            areatext.selectAll();
+        } else if(command.equals(about.getActionCommand())) {
+            JOptionPane.showMessageDialog(this, "This is a simple Notepad App\nCreated with Java and Swing", "About", JOptionPane.INFORMATION_MESSAGE);
+        }
     }
 }
